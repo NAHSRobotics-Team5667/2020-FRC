9c035ddd16a5e201551eb329870dcff9cfcde4e6
Gmaster257 <therealgamemaster257@gmail.com>
2020-01-11 19:33:15 -0500

    Fixed tab spacing, removed unnecessary imports, updated execute method

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -8,45 +8,49 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-////import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.RobotContainer;
 
 public class ShooterCommand extends CommandBase {
-  /**
-   * Creates a new ShooterCommand.
-   */
-  public ShooterCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
-
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    if (RobotContainer.getController().getAButton() == true) {
-      // ShooterSubsystem.fire();
-    } else if (RobotContainer.getController().getAButton() == false) {
-      // ShooterSubsystem.stopFire();
-    }
-    if (RobotContainer.getController().getLeftTrigger() >= 2) {
+    /**
+     * Creates a new ShooterCommand.
+     */
+    public ShooterCommand() {
+        // Use addRequirements() here to declare subsystem dependencies.
 
     }
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        if (RobotContainer.getController().getAButton() == true) {
+            // Fires the shooting wheels when A is pressed
+            RobotContainer.getShooterSubsystem().fire(0, 0); /** Replace 0s with proper values **/
+        } else if (RobotContainer.getController().getAButton() == false) {
+            // Stops firing the shooting wheels when A is released
+            RobotContainer.getShooterSubsystem().stopFire();
+        }
+        if (RobotContainer.getController().getLeftTrigger() >= 2) {
+            /**
+             * Put trigger code here. Confirm if this needs to be L or R trigger. Also
+             * replace "2" for a proper value.
+             **/
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
