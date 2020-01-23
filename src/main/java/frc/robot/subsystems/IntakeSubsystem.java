4fffc51e01fe314cca6ea1838b8006defb25a117
Gmaster257 <therealgamemaster257@gmail.com>
2020-01-23 18:18:15 -0500

    Added imports, cleaned up code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,60 +1,59 @@
 
-
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private SpeedController m_lWheel, m_rWheel;
+  private SpeedController m_intake;
+  private Solenoid m_rSolenoid, m_lSolenoid;
+  private SpeedController m_belt;
+  private Ultrasonic m_ultrasonic;
 
-    private SpeedControllerGroup m_intake;
+  private boolean previous, current = false;
 
-    private Ultrasonic m_ultrasonic;
-
-    private Solenoid m_rSolenoid, m_lSolenoid;
-
-    private int index = 3;
-
-    private boolean previous, current = false;
-
-  public IntakeSubsystem(SpeedController belt, SpeedController intake, Solenoid rSolenoid, Solenoid lSolenoid,
-   Ultrasonic ultrasonic) {
-
-    m_belt = belt;
-
+  /**
+   * Creates an intake subsystem instance
+   * 
+   * @param intake     - motor controller for the intake wheels
+   * @param rSolenoid  - right piston for extending/retracting intake
+   * @param lSolenoid  - left piston for extending/retracting intake
+   * @param belt       - motor controller for the belt
+   * @param ultrasonic - ultrasonic sensor that detects if the belt needs to be
+   *                   activated
+   */
+  public IntakeSubsystem(SpeedController intake, Solenoid rSolenoid, Solenoid lSolenoid, SpeedController belt,
+      Ultrasonic ultrasonic) {
     m_intake = intake;
-
     m_rSolenoid = rSolenoid;
-
     m_lSolenoid = lSolenoid;
-
+    m_belt = belt;
     m_ultrasonic = ultrasonic;
-
-  }
-  public void intake(){
-      rSolenoid.set(true);
-
-      lSolenoid.set(true);
-
-      intake.setSpeed(1.0);
-
   }
 
-  public void retractIntake(){
-
-       rSolenoid.set(false);
-
-       lSolenoid.set(false);
-
-       intake.setspeed(0);
+  /**
+   * Extends the intake outside of frame perimeter
+   */
+  public void extendIntake() {
+    m_rSolenoid.set(true);
+    m_lSolenoid.set(true);
+    m_intake.set(1.0);
   }
 
-  public boolean hasSeenBall(){
-    m_ultrasonic.
-    
+  /**
+   * Retracts the intake inside of frame perimeter
+   */
+  public void retractIntake() {
+    m_rSolenoid.set(false);
+    m_lSolenoid.set(false);
+    m_intake.set(0);
   }
-  
+
+  //// public boolean hasSeenBall(){}
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
