f1f1a3315e66ccb387f87df64a5995f75bdf192c
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2020-02-02 19:49:16 -0500

    Set only drivetrain code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,12 +28,7 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.commands.DriveTrainCommand;
-import frc.robot.commands.PositionCommand;
-import frc.robot.commands.RotationCommand;
-import frc.robot.commands.ShooterCommand;
 import frc.robot.subsystems.DriveTrainSubsystem;
-import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.WheelSubsystem;
 import frc.robot.utils.Controller;
 
 /**
@@ -53,8 +48,6 @@ public class RobotContainer {
 	public boolean done = false;
 
 	private static DriveTrainSubsystem m_drive;
-	private static ShooterSubsystem m_shooter;
-	private static WheelSubsystem m_wheel;
 
 	/**
 	 * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -68,16 +61,6 @@ public class RobotContainer {
 				new WPI_TalonFX(Constants.DriveConstants.rightSlave),
 				new WPI_TalonFX(Constants.DriveConstants.leftSlave), new AHRS(SPI.Port.kMXP));
 
-		m_wheel = new WheelSubsystem(new WPI_TalonFX(Constants.WheelConstants.MOTOR),
-				new ColorSensorV3(Constants.WheelConstants.COLOR_SENSOR_PORT));
-
-		m_shooter = new ShooterSubsystem(new PWMTalonSRX(Constants.ShooterConstants.RIGHT_SHOOTER_PORT),
-				new PWMTalonSRX(Constants.ShooterConstants.LEFT_SHOOTER_PORT),
-				new Encoder(Constants.ShooterConstants.RIGHT_ENCODER_PORT_A,
-						Constants.ShooterConstants.RIGHT_ENCODER_PORT_B),
-				new Encoder(Constants.ShooterConstants.LEFT_ENCODER_PORT_A,
-						Constants.ShooterConstants.LEFT_ENCODER_PORT_B));
-
 		m_drive.setDefaultCommand(new DriveTrainCommand(m_drive));
 
 	}
@@ -90,12 +73,8 @@ public class RobotContainer {
 	 */
 	private void configureButtonBindings() {
 		Button x = new JoystickButton(getController(), Constants.ControllerConstants.BUTTON_X_PORT);
-		Button b = new JoystickButton(getController(), Constants.ControllerConstants.BUTTON_B_PORT);
-		Button a = new JoystickButton(getController(), Constants.ControllerConstants.BUTTON_A_PORT);
 
 		x.whenPressed(() -> m_drive.resetOdometry(trajectory.getInitialPose()));
-		b.whenPressed(new PositionCommand(m_wheel));
-		a.whenPressed(new RotationCommand(m_wheel));
 	}
 
 	/**
