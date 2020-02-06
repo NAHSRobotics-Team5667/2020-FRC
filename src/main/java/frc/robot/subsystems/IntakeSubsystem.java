0d51dd93fd1287254bf7130837b3a32a69b7811c
Gmaster257 <therealgamemaster257@gmail.com>
2020-02-05 21:35:30 -0500

    Updated to support Rev2mDistanceSensors

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,36 +3,42 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.revrobotics.Rev2mDistanceSensor;
+import com.revrobotics.Rev2mDistanceSensor.Unit;
 
 public class IntakeSubsystem extends SubsystemBase {
 
 	private SpeedController m_intake;
 	private Solenoid m_rSolenoid, m_lSolenoid;
 	private SpeedController m_belt;
-	private Ultrasonic m_ultrasonic;
+	private Rev2mDistanceSensor m_intakeSensor;
+	private Rev2mDistanceSensor m_shooterSensor;
 
-	private boolean seenBall = false;
+	private boolean seenBallEnter = false;
+	private boolean seenBallExit = true;
 
 	/**
 	 * Creates an intake subsystem instance
 	 * 
-	 * @param intake     - motor controller for the intake wheels
-	 * @param rSolenoid  - right piston for extending/retracting intake
-	 * @param lSolenoid  - left piston for extending/retracting intake
-	 * @param belt       - motor controller for the belt
-	 * @param ultrasonic - ultrasonic sensor that detects if the belt needs to be
-	 *                   activated
+	 * @param intake        - motor controller for the intake wheels
+	 * @param rSolenoid     - right piston for extending/retracting intake
+	 * @param lSolenoid     - left piston for extending/retracting intake
+	 * @param belt          - motor controller for the belt
+	 * @param intakeSensor  - Rev2mDistanceSensor that detects if a ball has entered
+	 *                      the robot
+	 * @param shooterSensor - Rev2mDistanceSensor that detects if a ball has exited
+	 *                      the robot
 	 */
 	public IntakeSubsystem(SpeedController intake, Solenoid rSolenoid, Solenoid lSolenoid, SpeedController belt,
-			Ultrasonic ultrasonic) {
+			Rev2mDistanceSensor intakeSensor, Rev2mDistanceSensor shooterSensor) {
 		m_intake = intake;
 		m_rSolenoid = rSolenoid;
 		m_lSolenoid = lSolenoid;
 		m_belt = belt;
-		m_ultrasonic = ultrasonic;
+		m_intakeSensor = intakeSensor;
+		m_shooterSensor = shooterSensor;
 	}
 
 	/**
@@ -54,17 +60,35 @@ public class IntakeSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Determine if the ultrasonic sensor sees a power cell or not
+	 * Determine if the Rev2mDistanceSensor saw a power cell enter the robot or not
 	 * 
-	 * @return true if ball is seen
+	 * @return true if ball is present, therefore seen entering robot
 	 */
-	public boolean hasSeenBall() {
-		if (m_ultrasonic.getRangeInches() <= 6 && seenBall == false) {
-			seenBall = true;
-		} else if (m_ultrasonic.getRangeInches() > 6 && seenBall == true) {
-			seenBall = false;
+	public boolean hasSeenBallEnter() {
+		if (m_intakeSensor.getRange(Unit.kInches) <= Constants.IntakeConstants.SENSOR_RANGE_INCHES
+				&& seenBallEnter == false) {
+			seenBallEnter = true;
+		} else if (m_intakeSensor.getRange(Unit.kInches) > Constants.IntakeConstants.SENSOR_RANGE_INCHES
+				&& seenBallEnter == true) {
+			seenBallEnter = false;
 		}
-		return seenBall;
+		return seenBallEnter;
+	}
+
+	/**
+	 * Determine if the Rev2mDistanceSensor saw a power cell leave the robot or not
+	 * 
+	 * @return true if ball is absent, therefore seen leaving robot
+	 */
+	public boolean hasSeenBallExit() {
+		if (m_shooterSensor.getRange(Unit.kInches) <= Constants.IntakeConstants.SENSOR_RANGE_INCHES
+				&& seenBallExit == true) {
+			seenBallExit = false;
+		} else if (m_shooterSensor.getRange(Unit.kInches) > Constants.IntakeConstants.SENSOR_RANGE_INCHES
+				&& seenBallExit == false) {
+			seenBallExit = true;
+		}
+		return seenBallExit;
 	}
 
 	/**
